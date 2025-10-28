bac9332cd051efe337587bd942cc0c5a3f181049
TanmayM <tanmaylyn285@gmail.com>
2025-10-28 15:19:27 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from './components/navbar'
+import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import Video from './pages/Video'
 import {Routes, Route} from 'react-router-dom'
