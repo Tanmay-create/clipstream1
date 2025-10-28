d606ce06ddcb53e6cca81c942afe7e0b549865b5
TanmayM <tanmaylyn285@gmail.com>
2025-10-28 15:21:36 +0530

    fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Sidebar from '../components/sidebar'
+import Sidebar from '../components/Sidebar'
 import Feed from '../components/Feed'
 import '../css/Home.css'
 
